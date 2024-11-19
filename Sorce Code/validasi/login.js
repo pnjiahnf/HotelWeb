799f54df164520c1f9982a7c570f9689e2c9be33
pnjiahnf <panjiputra918@gmail.com>
2024-11-19 15:15:10 +0700

    New Update

---
+++ Sorce Code/validasi/login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+
+  
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  
+  if (validateLogin(email, password)) {
+    alert("Login berhasil! Anda akan diarahkan ke homepage.");
+    window.location.href = "homepage.html"; 
+  } else {
+    alert("Email atau password salah! Silakan coba lagi.");
+  }
+});
+
+function validateLogin(email, password) {
+  
+  const validEmail = "panji@gmail.com";
+  const validPassword = "silit123";
+
+  
+  return email === validEmail && password === validPassword;
+}
